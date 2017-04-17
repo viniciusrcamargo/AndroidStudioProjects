052a21406bcce6bc7356b9a39559431adbae200e
Vinicius Rodrigues Camargo <viniciusr.camargo@gmail.com>
2017-04-16 23:35:47 -0300

    Starting with Android Studio

---
+++ Notas/app/src/main/java/vinicius/com/notas/NotasActivity.java
@@ -0,0 +1,13 @@
+package vinicius.com.notas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notas);
+    }
+}
