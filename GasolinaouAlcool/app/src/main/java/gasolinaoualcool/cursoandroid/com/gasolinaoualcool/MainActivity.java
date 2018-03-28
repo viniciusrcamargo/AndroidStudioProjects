f594cd4da941677bee1f78ff14dfcd2889dbd235
Vinicius <viniciusr.camargo@gmail.com>
2018-03-28 13:10:04 -0300

    Continues

--- GasolinaouAlcool/app/src/main/java/gasolinaoualcool/cursoandroid/com/gasolinaoualcool/MainActivity.java
+++ GasolinaouAlcool/app/src/main/java/gasolinaoualcool/cursoandroid/com/gasolinaoualcool/MainActivity.java
@@ -1,11 +1,13 @@
 package gasolinaoualcool.cursoandroid.com.gasolinaoualcool;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +15,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText edtPrecoGasolina;
     private Button btnVerificar;
     private TextView txvResultado;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,15 +28,23 @@ public class MainActivity extends AppCompatActivity {
         btnVerificar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double precoAlcool =   Double.parseDouble(String.valueOf(edtPrecoAlcool.getText()));
-                double precoGasolina = Double.parseDouble(String.valueOf(edtPrecoGasolina.getText()));
+                double precoAlcool =   Double.parseDouble(String.valueOf(edtPrecoAlcool.getText().toString()));
+                double precoGasolina = Double.parseDouble(String.valueOf(edtPrecoGasolina.getText().toString()));
                 double calculo = precoAlcool / precoGasolina;
 
-                if(calculo < 0.7){
+
+                if(calculo <=0.7){
                     txvResultado.setText("É melhor abastecer com álcool");
                 }else{
                     txvResultado.setText("É melhor abastecer com gasolina");
                 }
+
+                Context context = getApplicationContext();
+                CharSequence text = "Você Clicou no botão verificar!!";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast clicouBotao = Toast.makeText(context, text, duration);
+                clicouBotao.show();
             }
         });
     }
